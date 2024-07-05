bde5828a00b3ac25aa76c7762772a780868d198e
Manoj-0809 <159109533+Manoj-0809@users.noreply.github.com>
2024-07-05 17:08:57 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,19 +1,9 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "virat"
-  password             = "virat1818"
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-}
+
 resource "aws_instance" "instance1" {
   ami                         = "ami-0195204d5dce06d99"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "test"
+  key_name                    = "kohli"
   vpc_security_group_ids     = ["sg-0640ac226b79673e7"]
   subnet_id                   = "subnet-001dafb658ac7d5b2"
   associate_public_ip_address = true
